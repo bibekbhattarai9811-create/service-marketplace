bd8a59f1a73b574bda6077541e0eabe4060db38c
bibekbhattarai9811-create <bibekbhattarai9811@gmail.com>
2026-03-21 01:55:50 -0500

    force reset database again

--- backend/main.py
+++ backend/main.py
@@ -41,6 +41,7 @@ app.add_middleware(
 # -----------------------------
 # Reset & Create Tables
 # -----------------------------
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 # -----------------------------
