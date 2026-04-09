f381ea27e52d04ad354af82f8a6e2ab3ebd0e786
bibekbhattarai9811-create <bibekbhattarai9811@gmail.com>
2026-04-09 12:58:55 -0500

    Shorten avatar migration revision id

--- backend/alembic/versions/0002_add_avatar_url_to_user_profiles.py
+++ backend/alembic/versions/0002_add_avatar_url_to_user_profiles.py
@@ -1,6 +1,6 @@
 """add avatar url to user profiles
 
-Revision ID: 0002_add_avatar_url_to_user_profiles
+Revision ID: 0002_avatar_url
 Revises: 0001_initial_schema
 Create Date: 2026-04-09 00:10:00.000000
 """
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0002_add_avatar_url_to_user_profiles"
+revision = "0002_avatar_url"
 down_revision = "0001_initial_schema"
 branch_labels = None
 depends_on = None
